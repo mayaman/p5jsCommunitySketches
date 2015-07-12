defcf34c0b3e5ac5ae43be2e3fbae73363cd38bc
Job van der Zwan <j.l.vanderzwan@gmail.com>
2015-07-12 13:43:44 +0300

    Implemented first version of first idea
    Yeah... I should have committed more often... sorry.
    
    * Created a Drifter class for the particles
      * Added states Drifters can be in. They can:
        * drift freely while being left alone
        * drift freely and be followed by befriended drifters
        * be befriended and follow free drifters to befriend them
        * be unavailable
      * Added draw code for the Drifter class
        * It squiggles!
        * It draws a network of befriended fellow drifters!
      * Added movement code for the Drifter class
        * When drifting, it semi-randomly floats around
        * When befriended, it's chasing potential new friends (tweens between current location and potential new friend)
      * Added code for finding new friends
    
    * Created an Asterisk class (to represent p5.js) that inherits from the Drifter class
      * The Asterisk class has different draw code. Also, it always starts out looking for friends

--- 0.25-0.33/0.25-0.33.js
+++ 0.25-0.33/0.25-0.33.js
@@ -36,17 +36,256 @@
 */
 
 var s = function( p ) {
+  var particles, p5Asterisk;
+
+  var cx, cy, cradius;
+
+  // A drifter is either freely drifting around,
+  // being followed by the p5 Asterisk or one
+  // of its friends, a friend of the asterisk,
+  // or not available
+  var DRIFTING = 0;
+  var FOLLOWED = 1;
+  var FRIENDED = 2;
+  var NOTAVAILABLE = 3;
 
   p.setup = function() {
-    // put setup code here
     p.createCanvas(p.windowWidth, p.windowHeight);
     p.background(255, 255, 255);
+
+    cx = p.width/4;
+    cy = p.height/2;
+    cradius = p.min(cx, cy)/2;
+
+    particles = new Array(100);
+    for (var i = particles.length - 1; i >= 0; i--) {
+    	particles[i] = new Drifter();
+    };
+
+    p5Asterisk = new Asterisk();
+    p.imageMode(p.CENTER);
   };
 
   p.draw = function() {
-    // put draw code here
-    
+    p.background(255, 255, 255);
+    for (var i = particles.length - 1; i >= 0; i--) {
+      particles[i].move();
+    };
+
+    // Draw the network in the background
+    for (var i = particles.length - 1; i >= 0; i--) {
+      particles[i].drawNetwork();
+    };
+    p5Asterisk.drawNetwork();
+
+    for (var i = particles.length - 1; i >= 0; i--) {
+      if (particles[i].state != FRIENDED){
+        particles[i].draw();
+      }
+    };    
+    // Draw the friended particles in the foreground
+    for (var i = particles.length - 1; i >= 0; i--) {
+      if (particles[i].state == FRIENDED){
+        particles[i].draw();
+      }
+    };    
+
+    p5Asterisk.move().draw();
   };
+
+
+  // ========== DRIFTER ==========
+
+  var Drifter = function () {
+
+  	this.state = DRIFTING;
+
+  	// colour
+  	this.r = p.random(255);
+  	this.g = p.random(255);
+  	this.b = p.random(255);
+
+  	// sets first position within a circle
+  	// make sure cx, cy and cradius are set before creating a Drifter!
+  	var t = p.random(p.TAU);
+  	var r = p.random(cradius/2, cradius*2);
+  	this.x = cx + r * Math.sin(t);
+  	this.y = cy + r * Math.cos(t);
+
+  	// store motion in polar coordinates
+  	this.theta = p.random(p.TAU);
+  	this.dtheta = p.random(-0.01, 0.01);
+  	this.ddtheta = p.random(-0.05 - this.dtheta * 0.1, 0.05 - this.dtheta * 0.1);
+  	this.v = p.random(1, 2);
+  	this.dx = Math.sin(this.theta) * this.v;
+  	this.dy = Math.cos(this.theta) * this.v;
+
+    // make it a random squiggle
+  	this.vertices = new Float32Array(14);
+  	for (var i = 0; i < this.vertices.length; i += 2) {
+  		// re-using t & r
+  		t = p.TAU* (p.random(2) + i) / this.vertices.length;
+  		r = p.random(5, 20);
+  		this.vertices[i]     = r * Math.sin(t);
+  		this.vertices[i + 1] = r * Math.cos(t);
+  	};
+
+  	this.tvertices = new Float32Array(this.vertices.length);
+  	for (var i = 0; i < this.tvertices.length; i += 2) {
+  		// re-using t & r
+  		t = p.TAU * (p.random(2) + i) / this.tvertices.length;
+  		r = p.random(5, 30);
+  		this.tvertices[i]     = r * Math.sin(t);
+  		this.tvertices[i + 1] = r * Math.cos(t);
+  	};
+
+  	this.verticeCounter = p.random(10, 20);
+
+  	this.friends = new Array(0);
+
+  	this.nextFriend = null;
+  };
+
+  Drifter.prototype.draw = function() {
+  	var alpha = 128;
+    if (this.state == FRIENDED){
+      alpha = 255;
+    } else if (this.state == NOTAVAILABLE){
+      alpha = 64;
+    }
+    p.fill(this.r, this.g, this.b, alpha);
+  	
+  	p.noStroke();
+  	p.beginShape();
+  	for (var i = 0; i < this.vertices.length; i += 2) {
+      p.curveVertex(this.x + this.vertices[i], this.y + this.vertices[i+1]);
+  	};
+    p.curveVertex(this.x + this.vertices[0], this.y + this.vertices[1]);  	
+    p.curveVertex(this.x + this.vertices[2], this.y + this.vertices[3]);  	
+    p.curveVertex(this.x + this.vertices[4], this.y + this.vertices[5]); 	
+    p.endShape();
+
+  	return this;
+  };
+
+  Drifter.prototype.drawNetwork = function() {
+    if (this.state == FRIENDED){
+      p.stroke(this.r, this.g, this.b);
+      p.strokeWeight(1);
+      for (var i = this.friends.length - 1; i >= 0; i--) {
+      	p.line(this.x, this.y, this.friends[i].x, this.friends[i].y);
+      };
+    }
+    return this;
+  }
+
+  Drifter.prototype.move = function() {
+  	if (this.state == DRIFTING || this.state == FOLLOWED || this.state == NOTAVAILABLE || this.nextFriend == null){
+  	  this.drift();
+  	} else if (this.state == FRIENDED){
+      this.follow();
+  	}
+  	return this;
+  }
+
+  Drifter.prototype.drift = function() {
+  	// random acceleration with negative feedback to angular velocity
+  	this.ddtheta = p.random(-0.1 - this.dtheta * 0.1, 0.1 - this.dtheta * 0.1);
+  	this.dtheta = this.dtheta + this.ddtheta;
+  	this.theta = (this.theta + this.dtheta + p.TAU) % p.TAU;
+
+  	// convert polar to carthesian coordinates
+   	this.dx = Math.sin(this.theta) * this.v;
+  	this.dy = Math.cos(this.theta) * this.v; 	
+    // add velocity to position, gently nudge towards (cx, cy)
+  	this.x = this.x + this.dx + (cx - this.x)*0.002;
+  	this.y = this.y + this.dy + (cy - this.y)*0.002;
+
+    this.squiggleVertices();
+
+  	return this;
+  }
+
+  // Makes it look like they're talking :)
+  Drifter.prototype.squiggleVertices = function() {
+    
+  	for (var i = this.vertices.length - 1; i >= 0; i -= 1) {
+  	  this.vertices[i] = 0.8125*this.vertices[i] + 0.1875*this.tvertices[i];
+    }
+    this.verticeCounter -= 1;
+    if (this.verticeCounter < 0){
+      for (var i = 0; i < this.tvertices.length; i += 2) {
+  	    var t = p.TAU* (p.random(2) + i) / this.tvertices.length;
+  	    var r = p.random(2, 30);
+  	    this.tvertices[i]     = r * Math.sin(t);
+  	    this.tvertices[i + 1] = r * Math.cos(t);
+  	  };
+  	 this.verticeCounter = p.random(5, 20);
+    }  	
+  }
+
+  Drifter.prototype.follow = function() {
+    if (this.nextFriend != null){
+    	this.x = this.x * 0.8125 + this.nextFriend.x * 0.1875;
+    	this.y = this.y * 0.8125 + this.nextFriend.y * 0.1875;
+
+    	var dx = (this.x - this.nextFriend.x);
+    	var dy = (this.y - this.nextFriend.y);
+    	// if less than 15 pixels away from nextFriend
+    	if (dx*dx + dy*dy < 225){
+          if (p.random(1) < 0.5){ // 1 in 2 chance of befriending
+          	this.nextFriend.state = FRIENDED;
+            this.nextFriend.findTarget();
+            this.friends.push(this.nextFriend);
+          } else {
+            this.nextFriend.state = NOTAVAILABLE;
+          }
+
+          this.findTarget();
+    	}
+    }
+    return this;
+  }
+
+  Drifter.prototype.findTarget = function() {
+    // Filter out the particles who are not freely drifting
+    var potentialFriends = new Array(0);
+    for (var i = particles.length - 1; i >= 0; i--) {
+    	if (particles[i].state == DRIFTING){
+    		potentialFriends.push(particles[i]);
+    	}
+    };
+
+    if (potentialFriends.length > 0){
+      var index = Math.floor( p.random(potentialFriends.length) );
+      this.nextFriend = potentialFriends[index];
+      this.nextFriend.state = FOLLOWED;
+    } else {
+      this.nextFriend = null;
+    }
+  }
+
+  // ========== ASTERISK ==========
+  // Inherits from Drifter
+  // Is the first to look for friends
+
+  var Asterisk = function () {
+  	Drifter.call(this);
+  	this.img = p.loadImage("p5-asterisk.svg");
+    //p5 is always looking for friends!
+  	this.state = FRIENDED;
+    this.findTarget();
+  }
+
+  Asterisk.prototype = Object.create(Drifter.prototype);
+
+  Asterisk.prototype.constructor = Asterisk;
+
+
+  Asterisk.prototype.draw = function(){
+    p.image(this.img, this.x, this.y);
+  }
+
 };
 
 var myp5 = new p5(s);
